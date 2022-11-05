935aceb30ad76d02247efaacab43ebfe79d6bce0
Antonets Kristina <kissuha2015@yandex.ru>
2022-11-06 00:02:03 +0300

    Seminar2_Task_10

---
+++ Seminar2_Task_10/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int GetTwoDigit(int number)
+        {
+            number = number / 10;
+            return number % 10;
+        }
+
+        Console.WriteLine("Введедите трехзначное число");
+        string inputString = Console.ReadLine();
+        int num = int.Parse(inputString);
+        if (num > 99)
+        {
+            if(num < 1000)
+            {
+                Console.WriteLine($"вторая цифра введенного числа: {GetTwoDigit(num)}");
+            }
+            else 
+            {
+                Console.WriteLine("Ошибка! Введено не корректное значение.");
+            }
+        }
+        else 
+        {
+            Console.WriteLine("Ошибка! Введено не корректное значение.");
+        }
+    }
+}
