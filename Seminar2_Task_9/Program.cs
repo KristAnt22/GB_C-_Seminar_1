c4830ea4468ecf14b619b9606361c3246c0d4eb2
Antonets Kristina <kissuha2015@yandex.ru>
2022-11-06 06:14:32 +0300

    Seminar2_Task_9

---
+++ Seminar2_Task_9/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Дополнительная Задача 9
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да   */
+
+string Maltiplicity(int num)
+{
+    if ((num % 7 == 0) && (num % 23 == 0))
+    {
+        Console.WriteLine($"Число {num} кратно и 7 и 23 одновременно");
+    }
+    else
+    {
+        Console.WriteLine("НЕТ");
+    }
+    return ""; 
+}
+
+Console.WriteLine("Введите число");
+String inputString = Console.ReadLine()!;   
+int number = int.Parse(inputString)!;  
+Console.WriteLine(Maltiplicity(number));
+
