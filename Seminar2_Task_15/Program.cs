efd169794111d685369f5800b7b9481ac050a1da
Antonets Kristina <kissuha2015@yandex.ru>
2022-11-06 05:37:46 +0300

    Seminar2_Task_15

---
+++ Seminar2_Task_15/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет   */
+
+string Weekend(int num)
+{
+    if (num > 0 && num < 8)
+    {
+        if ((num == 7) || (num == 6))
+        {
+            Console.WriteLine("Выходной!");
+        }
+        else
+        {
+            Console.WriteLine("Рабочий день.");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ошибка! Введите цифру от 1 до 7.");
+    }
+    return ""; 
+}
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+String inputString = Console.ReadLine()!;   
+int number = int.Parse(inputString)!;  
+Console.WriteLine(Weekend(number));
